497ebbf9de4c378b0ddd282c644b38ca9fd32f45
zhsa2001 <72894249+zhsa2001@users.noreply.github.com>
2025-02-28 09:14:15 +0300

    Added defaultReceiverApplicationId

--- app/src/main/java/com/zhsa2001/chromecast/CastOptionsProvider.kt
+++ app/src/main/java/com/zhsa2001/chromecast/CastOptionsProvider.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
 import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.CastMediaControlIntent
 
 class CastOptionsProvider: OptionsProvider {
 
     override fun getCastOptions(context: Context): CastOptions {
         return CastOptions.Builder()
+            .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
             .build()
     }
 
